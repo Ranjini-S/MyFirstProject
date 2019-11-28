7bf603c3a30c92b0394f4e1a317dcfcaae78bb2a
Ranjini-S <58292721+Ranjini-S@users.noreply.github.com>
2019-11-28 15:43:48 +0530

    Another Display added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,10 @@
+
 public class HelloWorld
 {
   public static void main(String [] args)
   {
     system.out.println("hello world from GITHUB");
+    system.out.println("My first Java program");
     system.out.println("Bye for now!");
   }
 }
